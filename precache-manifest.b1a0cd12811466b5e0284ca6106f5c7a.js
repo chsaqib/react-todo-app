3d70759fe03620ce1ab8e32639c52caf9b0582ec
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-13 21:24:58 +0200

    Updates

---
+++ precache-manifest.b1a0cd12811466b5e0284ca6106f5c7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e54e6cffe6830f751db",
+    "url": "/weather-scanner/static/js/runtime~main.1e54e6cf.js"
+  },
+  {
+    "revision": "8378d72c4c47d57271f5",
+    "url": "/weather-scanner/static/js/main.8378d72c.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/weather-scanner/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "8378d72c4c47d57271f5",
+    "url": "/weather-scanner/static/css/main.d7ee612c.chunk.css"
+  },
+  {
+    "revision": "30c9b70a9f11997cce90f5273ef729c8",
+    "url": "/weather-scanner/index.html"
+  }
+];
